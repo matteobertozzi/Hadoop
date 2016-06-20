807293a3297f954671037569990a1e362cb7664b
Wes Widner <kai5263499@gmail.com>
2016-06-20 15:41:09 -0400

    change toString to use mod 256 to make the method more generally applicable

--- python-hadoop/hadoop/io/BytesWritable.py
+++ python-hadoop/hadoop/io/BytesWritable.py
@@ -28,4 +28,4 @@ class BytesWritable(AbstractValueWritable):
         self._value = data_input.readFully(size)
 
     def toString(self):
-        return ''.join(chr(x) for x in self._value if x > 31 or x == 9)
+        return ''.join(chr(x % 256) for x in self._value)
